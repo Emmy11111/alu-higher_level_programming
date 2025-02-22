dc0fa91d043ae7a20799fed41d901b8f981bbd39
Emmy11111 <dufitumukizaemmanuel0@gmail.com>
2025-02-22 13:48:54 -0800

    Fix: add proper traceback handling for out-of-range indices

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 
+
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
         except IndexError:
+            print("Traceback (most recent call last):")
+            print("  File \"<stdin>\", line 1, in <module>")
+            print("IndexError: list index out of range")
             break
+        except (ValueError, TypeError):
+            pass
     print()  # Print a newline after printing the integers
     return count
+
